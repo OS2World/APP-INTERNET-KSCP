3952056b1021c8aa77af086fea9317ea4b9d2796
KO Myung-Hun <komh@chollian.net>
2013-02-27 15:03:33 +0900

    Add and update a license

--- addrbookdlg.cpp
+++ addrbookdlg.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 by KO Myung-Hun <komh@chollian.net>
+ * Copyright (c) 2012-2013 by KO Myung-Hun <komh@chollian.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms,
