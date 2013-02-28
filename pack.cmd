04f610b236660f27338840cc85e6c12251a4bfe4
KO Myung-Hun <komh@chollian.net>
2013-02-28 15:35:17 +0900

    Update pack.cmd to include KPMLib sources

--- pack.cmd
+++ pack.cmd
@@ -1,7 +1,31 @@
 @echo off
+setlocal
+
+SET PACK_DIR=pack
+SET KSCP_SRC=src.zip
+SET KPMLIB_SRC=kpmlibsrc.zip
+SET KPMLIB_DIR=KPMLib
+
 gmake clean
 gmake RELEASE=1
-git export > src.zip
-zip kscp kscp.exe COPYING README README.eng src.zip
-del src.zip
+
+git export > %KSCP_SRC%
+
+cd KPMLib
+git export > ..\%KPMLIB_SRC%
+cd ..
+
+md %PACK_DIR%
+cd %PACK_DIR%
+unzip ..\%KPMLIB_SRC% -d %KPMLIB_DIR%
+del ..\%KPMLIB_SRC%
+zip -rpSm ..\%KSCP_SRC% %KPMLIB_DIR%
+cd ..
+rd %PACK_DIR%
+
+zip kscp kscp.exe COPYING README README.eng %KSCP_SRC%
+del %KSCP_SRC%
+
 call name.cmd
+
+endlocal
